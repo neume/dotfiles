981485c7fbe095d3a049fbe5a581ef46827c819e
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2024-04-21 13:23:11 +0800

    Add copilot

---
+++ nvim/lazy/lua/neume/plugins/copilot.lua
@@ -0,0 +1,7 @@
+return {
+  'github/copilot.vim',
+  config = function()
+    vim.g.copilot_no_tab_map = true
+    vim.keymap.set('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true, replace_keycodes = false })
+  end,
+}
